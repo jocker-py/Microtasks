f477cd6635fe90d543606db3808be457bf73cf69
jocker-py <buladzislau@gmail.com>
2023-03-31 17:06:46 +0300

    feat: add solution for useState task

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
+  const [number, setNumber] = useState(0);
+  const increment = () => setNumber(number + 1);
+  const reset = () => setNumber(0);
+  const decrement = () => setNumber(number - 1);
   return (
     <div className="App">
+      <h3>{number}</h3>
+      <div>
+        <button onClick={increment}>+</button>
+        <button onClick={reset}>0</button>
+        <button onClick={decrement}>-</button>
+      </div>
     </div>
   );
 }
